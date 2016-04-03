b99d55164d633881c2e4c4f408057026d51b6e7f
jared weinstein <jaredweinstein1@gmail.com>
2016-04-02 22:58:12 -0400

    loading screen

---
+++ cornStock/LoadingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoadingViewController.swift
+//  stockTalk
+//
+//  Created by jared weinstein on 4/2/16.
+//  Copyright © 2016 Jared Weinstein. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class LoadingViewController: UIViewController {
+    @IBOutlet weak var continueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        continueButton.hidden = true;
+        Model.sharedInstance.setup()
+        self.checkLoaded()
+    }
+    
+    func checkLoaded(){
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(5 * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), {
+                self.loaded()
+            }
+        )
+    }
+    
+    func loaded(){
+        continueButton.hidden = false;
+    }
+}
